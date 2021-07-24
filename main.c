88134bdbe1015e362dbfeb88942b9622b98243fa
MarkoBorkovic <themarkoborkovic@gmail.com>
2021-07-24 18:11:49 +0200

    Added basic opcode and updated gitignore

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdarg.h>
+
 #include <SDL2/SDL.h>
 
 #define _1B 	bool
@@ -10,13 +12,16 @@
 #define WINDOW_WIDTH 	640
 #define WINDOW_HEIGHT 	320
 
+#define GBUFF_SIZE 32*64
+
+#define VERBOSE_OPCODES 1
 
 SDL_Window *window;
 SDL_Renderer *renderer;
 
 
 	
-_1B 	gbuff[64*32]; 
+_1B 	gbuff[GBUFF_SIZE]; 
 
 _8B 	delay_timer;
 _8B	sound_timer;
@@ -43,6 +48,7 @@ bool init_graphics(){
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if(renderer == NULL){
 		printf("SDL renderer failed to initialise: %s\n", SDL_GetError());
+
 	}
 
 	SDL_RenderSetLogicalSize(renderer, 64, 32);
@@ -93,10 +99,29 @@ static inline void _60hz(int currentCycleTime){
 	}
 }
 
+static inline void exit_emu(){
+	destroy_graphics();
+	exit(0);
+}
+
+static inline void verbose_opcode(_16B PC, _16B opcode, char* message, ...){
+
+	#if(VERBOSE_OPCODES)
+	char buffer[256];
+	va_list args;
+	va_start(args, message);
+	vsnprintf(buffer, 255, message, args);
+	va_end(args);
+
+	printf("%03X: %04X %s\n", PC, opcode, buffer);
+	#endif
+}
 
 int main()
 {
+
 	
+
 	_8B	memory[4096];	 	
 	_16B 	opcode;			
 	_8B	V[16]; 		// registers, only 0x0 - 0xE are used, 0xF is used as a flag
@@ -129,6 +154,17 @@ int main()
 	// load fontset into interpreter part of the memory
 	memcpy(memory, fontset, 80);
  
+	char rom_location[200];
+	scanf("%s", rom_location);
+	FILE *romfptr = fopen(rom_location, "rb");
+	if(romfptr == NULL){
+		printf("Couldn't open rom file: %s\n", rom_location);
+		return 1; 
+	} 
+	size_t rom_size = fread(memory+0x200, sizeof(_8B), 4096-0x1FF, romfptr);
+	fclose(romfptr);
+	printf("Rom size: %ld\n", rom_size);
+
 	if(!init_graphics())
 		return 1;
 
@@ -136,7 +172,7 @@ int main()
 	PC = 0x200;
 	opcode = 0;
 	memset(V, 0, 16);
-	SP = 0;
+	SP = 1;
 	I = 0;
 
 
@@ -149,18 +185,46 @@ int main()
 		while(SDL_PollEvent(&event)){
 			switch(event.type){
 			case SDL_QUIT:
-				return 0;
+				exit_emu();
 			}		
 		}		
 		
 		currentCycleTime = SDL_GetTicks();
 		if(currentCycleTime - lastCycleTime >= 1000/cycles_per_second){
-			static a = 0;
-			if(a > 144){
-				a = 0;
-				printf("+1\n");
+
+			opcode = memory[PC] << 8 | memory[PC+1];
+			PC+=2;
+			printf("%X\n", opcode);
+			if(!opcode)
+				exit_emu();
+		
+			switch(opcode & 0xF000){
+			case 0: // opcodes starting with 0
+				switch(opcode & 0x00FF){
+				case 0xE0: // 0x00E0 - clear the screen
+					verbose_opcode(PC, opcode, "Clearing screen");
+					memset(gbuff, 0, GBUFF_SIZE);
+					break;
+				case 0xEE: // 0x00EE - return from subroutine call
+					PC = stack[SP-1];
+					verbose_opcode(PC, opcode, "Returning from subroutine to: %03X",stack[SP-1]);
+					SP--;
+					break;
+				}
+				break;
+
+			case 1: // 0x1NNN - jump to location NNN
+				_16B target_location = opcode & 0x0FFF;
+				PC = target_location;
+				verbose_opcode(PC, opcode, "Jumping to location: %03X", target_location);
+				break;
+			case 2: // 0x2NNN - jump to subroutine at address NNN
+				_16B target_subroutine = opcode & 0x0FFF;
+				stack[(SP++)-1] = PC+2;
+				PC = target_location;
+				verbose_opcode(PC, opcode, "Jumping to subroutine: %03X", target_subroutine);
+				break;
 			}
-			a++;			
 
 			lastCycleTime = currentCycleTime;
 		}
@@ -170,7 +234,7 @@ int main()
 		SDL_Delay(0);		
 	}
 
-
+	
 	destroy_graphics();	
 	return 0;
 }
